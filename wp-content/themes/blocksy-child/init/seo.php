a176a7e51cf3778bdf512ef300a1ee67d151082d
chunsa1615 <102647440+NagilMaster@users.noreply.github.com>
2025-06-17 12:57:37 +0900

    Update seo.php

--- wp-content/themes/blocksy-child/init/seo.php
+++ wp-content/themes/blocksy-child/init/seo.php
@@ -1,6 +1,7 @@
 <?php
 add_action("naver_search", function () {
     ?>
+    <meta name="naver-site-verification" content="66beb8ead4acfddf953df368f02eb217e6d25fea" />
     <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
     <script type="text/javascript">
         if(!wcs_add) var wcs_add = {};
